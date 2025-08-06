401c310845f59d63cc839e5ca5f5aa2767fc2b94
kaif00092 <kaifarshad07@gmail.com>
2025-08-06 14:28:38 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
+if (!process.env.NODE_ENV) {
+  console.warn("⚠️  Warning: NODE_ENV is not set. Check your .env formatting.");
+}
+
 //middleware
 if (process.env.NODE_ENV !== "production") {
   app.use(cors({ origin: "http://localhost:5173" }));
@@ -30,7 +34,7 @@ app.use("/api/notes", notesRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
